c5e00a369d4ccf8de5df38176783f7d55fd1b225
hippili <hippili@unomaha.edu>
2020-03-11 23:34:12 -0500

    deploy to heroku

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zlhw52b_p*u4dfa*ho17z6r=6p776hle&%s(i42zv@=jt((57!'
+SECRET_KEY = 'f30*)*hkl(3&g$_&!zlq8(o@_qf186=rzr^n=8k5zi*c8a#+j^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
